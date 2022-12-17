106be73085641d86a21cf7f360b6727ab8d41fea
dudest <34729881+dudest@users.noreply.github.com>
2022-12-17 17:13:42 -0500

    working
    file clean up. LevelControl seems to be working.

--- soundweb-nodes/soundweb-sever.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(RED) {
-    function SoundwebServerNode(n) {
-        RED.nodes.createNode(this,n);
-        this.host = n.host;
-        this.port = n.port;
-    }
-    RED.nodes.registerType("soundweb-server", SoundwebServerNode);
-}
